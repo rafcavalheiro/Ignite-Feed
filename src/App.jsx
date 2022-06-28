79c54850820a50e74e91891fc53dce2608fc82d7
rafcavalheiro <rafcavalheiro@gmail.com>
2022-06-27 21:42:37 -0300

    Implementando estilos no Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import { Header } from "./components/Header"
 import { Post } from "./Post"
+
+import './global.css'
 
 export function App() {  
 
   return (
 
     <div>
+
+      <Header/>
+      
       <Post 
     author="Rafael Cavalheiro" 
     content=" Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sapiente debitis deleniti rerum libero distinctio similique quisquam inventore hic, earum nihil labore excepturi maiores mollitia reiciendis quae quaerat modi error beatae."
