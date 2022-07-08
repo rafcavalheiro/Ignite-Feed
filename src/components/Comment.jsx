11aacbe2a140be1cf213ff300ae982c45ce80c44
rafcavalheiro <rafcavalheiro@gmail.com>
2022-07-08 01:27:22 -0300

    Configurando likes nos comentário

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,9 +1,16 @@
 import { HandsClapping, ThumbsUp, Trash } from 'phosphor-react'
+import { useState } from 'react';
 import { Avatar } from './Avatar'
 import styles from './Comment.module.css'
 
 export function Comment({content, onDeleteComment}){
 
+    const [likeCount, setLikeCount] = useState(0);
+
+    function handleLikeComment(){
+        setLikeCount(likeCount +1);
+    }
+    
     function handleDeleteComment(){
         onDeleteComment(content);
     }
@@ -29,8 +36,9 @@ export function Comment({content, onDeleteComment}){
                 </div>
 
                 <footer>
-                    <button> <ThumbsUp/> Aplaudir <span>20</span> 
-                    
+                    <button onClick={handleLikeComment}>
+                         <ThumbsUp/> 
+                         Aplaudir <span>{likeCount}</span>                     
                     </button>
                 </footer>
              </div>             
